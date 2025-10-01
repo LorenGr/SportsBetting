7abd2057586b250431e0a6a02b8f6fe7211b3afa
Loren Grixti <lorengrx@gmail.com>
2025-10-01 14:24:34 +0100

    Update Header.jsx
    update text

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,9 +5,9 @@ import Github from '../Icon/Github'
 export default function Header() {
   return (
     <header className="header">
-      <strong className="logo">SportX Senior Lead Frontend tech test</strong>
+      <strong className="logo">Sports Live Betting</strong>
       <div className="header-right">
-        <span className="badge">Test Results : Loren Grixti</span>
+     
         <ThemeSwitcher />
         <a href="https://github.com/LorenGr/SportsBetting" target="_blank" rel="noopener noreferrer">
           <Github />
